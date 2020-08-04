298b32e2e87216a67e3145b92faf0e9a8bf84d95
Lucas Lima <38673203+lucaslimaax@users.noreply.github.com>
2020-08-03 22:33:02 -0300

    Create Routes
    Create Landing Page
    Create of styles
    Definition of routes App

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import './styles.css';
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
@@ -21,15 +22,15 @@ function Landing() {
               />
 
             <div className="buttons-container">
-                <a href="" className="study">
+                <Link to="/study" className="study">
                     <img src={studyIcon} alt="Estudar" />
                     Estudar
-                </a>
+                </Link>
 
-                <a href="" className="give-classes">
+                <Link to="/give-classes" className="give-classes">
                     <img src={giveClassesIcon} alt="Dar aulas" />
                     Dar Aulas
-                </a>
+                </Link>
 
             </div>
 
